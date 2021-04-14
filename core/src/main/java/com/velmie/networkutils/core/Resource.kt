f09ea5268eae3c44487d39688fc6fdfbfe2d12b9
dmitrijmihajluk <mikhajlyuk@gmail.com>
2021-04-14 11:01:49 +0300

    refactor(core): remove status from Resource.kt

--- core/src/main/java/com/velmie/networkutils/core/Resource.kt
+++ core/src/main/java/com/velmie/networkutils/core/Resource.kt
@@ -2,26 +2,20 @@ package com.velmie.networkutils.core
 
 import com.velmie.parser.entity.parserResponse.ParserMessageEntity
 
-enum class Status {
-    SUCCESS,
-    ERROR,
-    LOADING
-}
-
 /**
  * A generic class that holds a value with its loading status.
  * @param <T>
 </T> */
-sealed class Resource<out T> constructor(open val status: Status, open val data: T?)
+sealed class Resource<out T> constructor(open val data: T?)
 
 class Loading<T>(override val data: T?, val progress: Float = 0f) :
-    Resource<T>(Status.LOADING, data)
+    Resource<T>(data)
 
-class Success<T>(override val data: T?) : Resource<T>(Status.SUCCESS, data)
+data class Success<T>(override val data: T?) : Resource<T>(data)
 
 class Error<T>(
     override val data: T?,
     val exception: Exception? = null,
     val errorMessage: String? = null,
     val errors: List<ParserMessageEntity<Int>>? = null
-) : Resource<T>(Status.ERROR, data)
+) : Resource<T>(data)
